228c39ddd67a6cd13c6b093ebf6071eb5ac973c8
Ali Husain Sorathiya <123808017+alihusains@users.noreply.github.com>
2026-05-03 13:01:11 +0400

    aa

---
+++ mac_init.sh
@@ -0,0 +1,59 @@
+# Install Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# Install GitHub CLI
+brew install gh
+# Install Node.js
+brew install node
+# Install Python
+brew install python
+# Install Visual Studio Code
+brew install --cask visual-studio-code
+# Install Google Chrome
+brew install --cask google-chrome
+# install VLC
+brew install --cask vlc
+# Install Slack
+brew install --cask slack
+# Install Zoom
+
+# Install Postman
+brew install --cask postman
+# Install Flutter
+brew install --cask flutter
+#install Android SDK
+brew install --cask android-sdk
+# Install Dart SDK
+brew install --cask dart-sdk
+# Install Android Studio
+brew install --cask android-studio
+
+# Install Zed IDE
+brew install --cask zed
+
+# Install Visual Studio Code extensions
+code --install-extension ms-python.python
+code --install-extension esbenp.prettier-vscode
+# install live server extension
+code --install-extension ritwickdey.LiveServer
+# install vs code reload  extension
+
+
+# Install Antigravity Claude Proxy
+npm install -g antigravity-claude-proxy@latest
+
+# Install Localsend 
+brew install --cask localsend
+
+# Install Raycast
+brew install --cask raycast
+
+# Install Wispr Flow
+brew install --cask wispr-flow
+
+# Install ADB
+brew install --cask android-platform-tools
+brew install adb-enhanced
+
+
+
+
